04912e65598d321db028967ce11d5f612c81aebe
RenanR1711 <renanrodriguesarruda17@hotmail.com>
2024-02-15 21:48:19 -0300

    ultima coisa rotas

--- app/routes/usersRoutes.js
+++ app/routes/usersRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const usersController = require("../controllers/usersController");
 
 router.post("/", usersController.createUsers);
+router.get("/", usersController.readAllUsers);
+router.put("/:id", usersController.updateUsers);
+router.put("/:id", usersController.deleteUsers);
 
 module.exports = router;
